ef23156f4eedad91f9e793c0c3a34964135f024f
teromonte8 <tar.monteiro@campus.fct.unl.pt>
2022-12-01 23:59:30 +0000

    first steps

---
+++ TP2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Auxiliary functions for assignment 2
+"""
+import numpy as np
+import tp2_aux as aux
+
+a = aux.images_as_matrix()
+b = np.loadtxt("labels.txt")
