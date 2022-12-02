4b58d79ec7b9beef8b0e5371ce41a4ad2279a06a
teromonte <tar.monteiro@campus.fct.unl.pt>
2022-12-02 10:24:06 +0000

    start

--- TP2.py
+++ TP2.py
@@ -5,6 +5,21 @@ Auxiliary functions for assignment 2
 """
 import numpy as np
 import tp2_aux as aux
+from sklearn.decomposition import KernelPCA
+from sklearn.decomposition import PCA
+from sklearn.manifold import Isomap
 
-a = aux.images_as_matrix()
-b = np.loadtxt("labels.txt")
+X = aux.images_as_matrix()
+labels = np.loadtxt("labels.txt", delimiter=",")
+
+
+pca = PCA(n_components=6)
+X_transformed_PCA = pca.fit_transform(X)
+
+transformer = KernelPCA(n_components=6, kernel='rbf')
+X_transformed_KPCA = transformer.fit_transform(X)
+
+embedding = Isomap(n_components=6)
+X_transformed_Isomap = embedding.fit_transform(X)
+
+res = np.concatenate((X_transformed_PCA, X_transformed_KPCA, X_transformed_Isomap), axis=1)
