938e47e5f3692e025a0fe1ea1ce53999a1b4c7c7
teromonte <tar.monteiro@campus.fct.unl.pt>
2022-12-02 19:49:40 +0000

    reports

--- TP2.py
+++ TP2.py
@@ -8,6 +8,9 @@ import tp2_aux as aux
 from sklearn.decomposition import KernelPCA
 from sklearn.decomposition import PCA
 from sklearn.manifold import Isomap
+from sklearn.cluster import KMeans
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import SpectralClustering
 
 X = aux.images_as_matrix()
 labels = np.loadtxt("labels.txt", delimiter=",")
@@ -24,7 +27,19 @@ X_transformed_Isomap = embedding.fit_transform(X)
 
 res = np.concatenate((X_transformed_PCA, X_transformed_KPCA, X_transformed_Isomap), axis=1)
 
-means = np.mean(res,axis=0)
-stdevs = np.std(res,axis=0)
-resStd = (res-means)/stdevs
+resSTD = (res - np.mean(res, axis=0)) / np.std(res, axis=0)
 
+# KMeans ##############################################
+
+labels_Kmeans = KMeans(n_clusters=4).fit_predict(resSTD)
+aux.report_clusters(labels[:,0], labels_Kmeans, "report_kmeans.html")
+
+# AgglomerativeClustering #############################
+
+labels_Agg = AgglomerativeClustering(n_clusters = 4).fit_predict(resSTD)
+aux.report_clusters(labels[:,0], labels_Agg, "report_agg.html")
+
+# SpectralClustering ##################################
+
+labels_Spec = SpectralClustering(n_clusters=4, assign_labels='cluster_qr').fit_predict(resSTD)
+aux.report_clusters(labels[:,0], labels_Spec, "report_spec.html")
