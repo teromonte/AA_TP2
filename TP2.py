d65e6851fc174f25cae6a5ddcdaa08352a3866d0
Jose <jn.cunha@campus.fct.unl.pt>
2022-12-21 16:26:13 +0000

    Comparisons based on cluster number

--- TP2.py
+++ TP2.py
@@ -4,6 +4,7 @@
 Auxiliary functions for assignment 2
 """
 import numpy as np
+import matplotlib.pyplot as plt
 import tp2_aux as aux
 from sklearn.decomposition import KernelPCA
 from sklearn.decomposition import PCA
@@ -16,6 +17,9 @@ from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics.cluster import contingency_matrix
+
+CLUSTERS_MIN = 2
+CLUSTERS_MAX = 10
 
 X = aux.images_as_matrix()
 labels = np.loadtxt("labels.txt", delimiter=",")
@@ -40,42 +44,111 @@ res = np.concatenate((X_transformed_PCA, X_transformed_KPCA, X_transformed_Isoma
 
 resSTD = (res - np.mean(res, axis=0)) / np.std(res, axis=0)
 
+fig = plt.figure(figsize=(18, 12))
+fig.subplots_adjust(hspace=0.22, wspace=0.25)
+
+metrics = {
+    "rankIndex": [],
+    "f1 score": [],
+    "recall": [],
+    "precision": [],
+    "purity": []
+  }
+
+k_aux = 1
+for m in metrics.keys():
+    # plotting one graph
+    ax = fig.add_subplot(2, 3, k_aux)
+    k_aux += 1
+    ax.set_title(m)
+    ax.set_xlabel('num of clusters')
+    ax.set_ylabel(m)
+    metrics[m] = [-1] * (CLUSTERS_MAX - CLUSTERS_MIN + 1)
+    
 # KMeans ##############################################
 
-labels_Kmeans = KMeans(n_clusters=4).fit_predict(resSTD)
-aux.report_clusters(labels[:,0], labels_Kmeans, "report_kmeans.html")
+for k in range(CLUSTERS_MIN, CLUSTERS_MAX+1):
+    array_pos = k - CLUSTERS_MIN
+    labels_Kmeans = KMeans(n_clusters=k).fit_predict(resSTD)
+    
+    score_randIndex = rand_score(labels[:,1], labels_Kmeans)
+    metrics["rankIndex"][array_pos] = score_randIndex
+    
+    score_f1 = f1_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["f1 score"][array_pos] = score_f1
+    
+    score_recall = recall_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["recall"][array_pos] = score_recall
 
-aKmeans_randIndex = rand_score(labels[:,1], labels_Kmeans)
-bKmeans_f1 = f1_score(labels[:,1], labels_Kmeans, average='macro')
-cKmeans_recall = recall_score(labels[:,1], labels_Kmeans, average='macro')
-dKmeans_precision = precision_score(labels[:,1], labels_Kmeans, average='macro')
-eKmeans_purity = purity_score(labels[:,1], labels_Kmeans)
+    score_precision = precision_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["precision"][array_pos] = score_precision
+
+    score_purity = purity_score(labels[:,1], labels_Kmeans)
+    metrics["purity"][array_pos] = score_purity
+    
+for ax in fig.get_axes():
+    ax.plot( range(CLUSTERS_MIN, CLUSTERS_MAX+1), metrics[ax.get_title()] ) 
+    
+best_k = 4 # how to calculate?
+aux.report_clusters(labels[:,0], KMeans(n_clusters=best_k).fit_predict(resSTD), "report_kmeans.html")
 
 # AgglomerativeClustering #############################
 
-labels_Agg = AgglomerativeClustering(n_clusters = 4).fit_predict(resSTD)
-aux.report_clusters(labels[:,0], labels_Agg, "report_agg.html")
+for k in range(CLUSTERS_MIN, CLUSTERS_MAX+1):
+    array_pos = k - CLUSTERS_MIN
+    labels_Kmeans = AgglomerativeClustering(n_clusters=k).fit_predict(resSTD)
+    
+    score_randIndex = rand_score(labels[:,1], labels_Kmeans)
+    metrics["rankIndex"][array_pos] = score_randIndex
+    
+    score_f1 = f1_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["f1 score"][array_pos] = score_f1
+    
+    score_recall = recall_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["recall"][array_pos] = score_recall
 
-aAgg_randIndex = rand_score(labels[:,1], labels_Agg)
-bAgg_f1 = f1_score(labels[:,1], labels_Agg, average='macro')
-cAgg_recall = recall_score(labels[:,1], labels_Agg, average='macro')
-dAgg_precision = precision_score(labels[:,1], labels_Agg, average='macro')
-eAgg_purity = purity_score(labels[:,1], labels_Agg)
+    score_precision = precision_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["precision"][array_pos] = score_precision
+
+    score_purity = purity_score(labels[:,1], labels_Kmeans)
+    metrics["purity"][array_pos] = score_purity
+    
+for ax in fig.get_axes():
+    ax.plot( range(CLUSTERS_MIN, CLUSTERS_MAX+1), metrics[ax.get_title()] ) 
+    
+best_k = 4 # how to calculate?
+aux.report_clusters(labels[:,0], AgglomerativeClustering(n_clusters=best_k).fit_predict(resSTD), 
+                    "report_kmeans.html")
+
 
 # SpectralClustering ##################################
 
-labels_Spec = SpectralClustering(n_clusters=4, assign_labels='cluster_qr').fit_predict(resSTD)
-aux.report_clusters(labels[:,0], labels_Spec, "report_spec.html")
+for k in range(CLUSTERS_MIN, CLUSTERS_MAX+1):
+    array_pos = k - CLUSTERS_MIN
+    labels_Kmeans = SpectralClustering(n_clusters=k, assign_labels='cluster_qr').fit_predict(resSTD)
+    
+    score_randIndex = rand_score(labels[:,1], labels_Kmeans)
+    metrics["rankIndex"][array_pos] = score_randIndex
+    
+    score_f1 = f1_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["f1 score"][array_pos] = score_f1
+    
+    score_recall = recall_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["recall"][array_pos] = score_recall
 
-aSpec_randIndex = rand_score(labels[:,1], labels_Spec)
-bSpec_f1 = f1_score(labels[:,1], labels_Spec, average='macro')
-cSpec_recall = recall_score(labels[:,1], labels_Spec, average='macro')
-dSpec_precision = precision_score(labels[:,1], labels_Spec, average='macro')
-eSpec_purity = purity_score(labels[:,1], labels_Spec)
+    score_precision = precision_score(labels[:,1], labels_Kmeans, average='macro')
+    metrics["precision"][array_pos] = score_precision
 
-# examine the performances varying the main parameters of each clustering algo
-
-
+    score_purity = purity_score(labels[:,1], labels_Kmeans)
+    metrics["purity"][array_pos] = score_purity
+    
+for ax in fig.get_axes():
+    ax.plot( range(CLUSTERS_MIN, CLUSTERS_MAX+1), metrics[ax.get_title()] ) 
+    
+best_k = 4 # how to calculate?
+aux.report_clusters(labels[:,0], SpectralClustering(n_cluster=best_k, assign_labels='clusters_qr').fit_predict(resSTD), 
+                    "report_kmeans.html")
+# examine the performances varying the main parameters of each clustering alg
 
 
 #internalIndex
